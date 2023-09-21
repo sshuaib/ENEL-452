bc2de4ef44c4e0152d19c2966c745176cdd95ce6
sshuaib <salmanshuaib18@gmail.com>
2023-09-21 10:29:00 -0600

    third commit - blinking led

--- labs/lab1/main.c
+++ labs/lab1/main.c
@@ -5,4 +5,14 @@ int main()
 {
 	gpioInit();	
 	ledOn();
+	
+	while(1) // Turn on PA5 led
+	{
+		GPIOA->ODR |= GPIO_ODR_ODR5;//turn on
+		delay(3500000);
+		GPIOA->ODR &= (uint32_t) ~GPIO_ODR_ODR5;//turn off
+		delay(3500000);
+	}
+	
+	return 0;
 }
